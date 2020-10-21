25c82e31d0c1fb507641f985608f5e530809f927
SBesedin <sbesedin@gorvodokanal.com>
2020-10-21 11:39:48 +0700

    entity style created

---
+++ src/Entity/Style.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="style")
+ */
+class Style
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $style_section_id;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStyleSectionId()
+    {
+        return $this->style_section_id;
+    }
+
+    /**
+     * @param mixed $style_section_id
+     */
+    public function setStyleSectionId($style_section_id): void
+    {
+        $this->style_section_id = $style_section_id;
+    }
+
+
+}
